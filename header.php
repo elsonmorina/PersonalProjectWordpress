b727211005f19d9ebe0a9a9676cefbb4585d1727
elsonmorina <167892030+elsonmorina@users.noreply.github.com>
2025-10-04 14:37:46 +0200

    day1

---
+++ header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+     <?php
+    wp_head();
+
+    ?> 
+</head>
+<?php
+ if(is_front_page()) {
+    $ds = array('ds-theme', 'my-class');
+ }else {
+    $ds = array('no-ds-theme');
+ }
+?>
+<body <?php body_class($ds )?>>
+
+   <?php
+
+     if(!is_page('landing-page')): ?>
+     <section class="menu-area">
+
+    <div class="container">
+        <nav class="main-menu">
+            <button class="check-button">
+                <div class="menu-icon">
+                    <div class="bar1"></div>
+                    <div class="bar2"></div>
+                    <div class="bar3"></div>
+                </div>
+            </button>
+            <?php  wp_nav_menu(array('theme_location' => 'wp_devs_main_menu', 'depth'=> 2));?>
+        </nav>
+    </div>
+     </section>
+
+     <?php endif; ?>
